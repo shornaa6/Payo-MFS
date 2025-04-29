658703a47b41cbdf7419ebf03bafbb5544eea725
shornaa6 <naharshorna16@gmail.com>
2025-04-29 08:16:05 +0600

    add amount section

--- login.js
+++ login.js
@@ -1,17 +1,27 @@
 // console.log("button clicking file added")
 
-document.getElementById("button-login").addEventListener("click:",function(){
-console.log("button clicked")
+// document.getElementById("button-login")
+// .addEventListener("click", function() {
+//   console.log("button clicked");
+// });
 
-})
 
-document.getElementById("button-login").addEventListener("click:",function(event){
+
+document.getElementById("button-login")
+.addEventListener("click:",function(event){
   event.preventDefault();
-  console.log("button clicked")
+//   console.log("button clicked")
     
     
 
 const phoneNUmber = document.getElementById("phone-number").value;
-console.log(phoneNUmber);
+const Pinnumber = document.getElementById("pin-number").value;
+console.log(phoneNUmber, Pinnumber);
+
+if (phoneNUmber === "5" || Pinnumber === "1234") {
+    console.log("you are logged in");
+    window.location.href = "/home.html";
+} else {
+    alert("invalid phone number or pin number");
+  }
 })
-    
